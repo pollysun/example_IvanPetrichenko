59652901190a98ce1c1fbbad11b143bd3b8bb9af
Oleg <example@gmail.com>
2020-12-10 22:41:05 +0300

    Cycles

--- js/script2.js
+++ js/script2.js
@@ -1,36 +1,21 @@
 ' use strict ';
 
-if (4 == 9) {
-  console.log('Ok!');
-} else {
-  console.log('Error');
-}
+let num = 50;
 
-
-
-// if (num < 49) {
-//   console.log('Error');
-// } else if (num > 100) {
-//   console.log('Много!');
-// } else {
-//   console.log('OK!');
+// while (num <= 55) {
+//   console.log(num);
+//   num++;
 // }
 
-// (num === 50) ? console.log('OK!') : console.log('Error');
+// do {
+//   console.log(num);
+//   num++;
+// }
+// while (num < 55);
 
-const num = 50;
-
-switch (num) {
-  case 49:
-    console.log('Неверно!');
+for (let i = 1; i < 10; i++) {
+  if (i === 6) {
     break;
-  case 100:
-    console.log('Много!');
-    break;
-  case 51:
-    console.log('Верно!');
-    break;
-  default:
-    console.log('Не в этот раз!');
-    break;
+  }
+  console.log(i);
 }
